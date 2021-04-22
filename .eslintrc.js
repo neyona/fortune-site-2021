f99988a3890cfee21e11b02f8cbd4b381a0c8750
neyona <40152041+neyona@users.noreply.github.com>
2021-04-22 15:36:55 -0400

    ES Linter

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+// .eslintrc.js
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unescaped-entities': 'off',
+  },
+}
