bbc3218d6d53105eddb7b5a06662670ccfa04e07
neyona <40152041+neyona@users.noreply.github.com>
2021-04-24 18:18:21 -0400

    Update with lint fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 // fortune_docker/frontend/src/index.js
-import React from 'react';
-import ReactDOM, { render } from 'react-dom';
-import App from './components/App';
-import './styles/main.scss';
+import React from 'react'
+import ReactDOM, { render } from 'react-dom'
+import App from './components/App'
+import './styles/main.scss'
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
