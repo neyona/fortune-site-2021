5576c93c5aec85755b7b4fc6d43e518370b2ddf5
neyona <40152041+neyona@users.noreply.github.com>
2021-04-24 17:03:24 -0400

    after linting

--- frontend/src/components/buttons/useDyslexiaTheme.js
+++ frontend/src/components/buttons/useDyslexiaTheme.js
@@ -1,70 +1,70 @@
-// fortune_docker/frontend/src/components/buttons/useDyslexiaTheme.js
-import React, { useState, useEffect, useContext } from 'react';
+// frontend/src/components/buttons/useDyslexiaTheme.js
+import React, { useState, useEffect, useContext } from 'react'
 
 const ThemeDyslexiaContext = React.createContext({
   isDyslexia: false,
-  toggleDyslexiaTheme: () => {}
-});
+  toggleDyslexiaTheme: () => {},
+})
 
 // Can only be used within the ThemeDyslexiaContext provider
 const useDyslexiaTheme = () => {
-  const context = useContext(ThemeDyslexiaContext);
+  const context = useContext(ThemeDyslexiaContext)
   if (!context) {
-    throw new Error('useDyslexiaTheme must be used within ThemeDyslexiaProvider');
+    throw new Error(
+      'useDyslexiaTheme must be used within ThemeDyslexiaProvider'
+    )
   }
-  return context;
-};
+  return context
+}
 
 // A custom hook to add ".dyslexia" class to the the body
-// element if the persisted mode on localStorage is dark.
+// element if the persisted mode on localStorage is dyslexia.
 const useDyslexiaThemeEffect = () => {
   const [themeDyslexiaState, setThemeDyslexiaState] = useState({
     isDyslexia: false,
-    hasDyslexiaThemeLoaded: false
-  });
-
+    hasDyslexiaThemeLoaded: false,
+  })
   useEffect(() => {
-    const lsDyslexia = localStorage.getItem('isDyslexia') === 'true';
+    const lsDyslexia = localStorage.getItem('isDyslexia') === 'true'
     if (lsDyslexia) {
-      document.querySelector('body').classList.add('dyslexia');
+      document.querySelector('body').classList.add('dyslexia')
     }
     setThemeDyslexiaState({
       ...themeDyslexiaState,
       isDyslexia: lsDyslexia,
-      hasDyslexiaThemeLoaded: true
-    });
-  }, []);
-
-  return { themeDyslexiaState, setThemeDyslexiaState };
-};
+      hasDyslexiaThemeLoaded: true,
+    })
+  }, [])
+  return { themeDyslexiaState, setThemeDyslexiaState }
+}
 
 const ThemeDyslexiaProvider = ({ children }) => {
-  const { themeDyslexiaState, setThemeDyslexiaState } = useDyslexiaThemeEffect();
-
+  const { themeDyslexiaState, setThemeDyslexiaState } = useDyslexiaThemeEffect()
   // Render <div /> if the mode is not loaded yet
   // to avoid rendering in the regular font by default
-  if (!themeDyslexiaState.hasDyslexiaThemeLoaded) return <div />;
-
+  if (!themeDyslexiaState.hasDyslexiaThemeLoaded) return <div />
   // Add or remove the dyslexia class from the body element
   // when a user toggles the switch
   const toggleDyslexiaTheme = () => {
-    const isDyslexia = !themeDyslexiaState.isDyslexia;
-    localStorage.setItem('isDyslexia', JSON.stringify(isDyslexia));
-    const bodyEl = document.querySelector('body');
-    isDyslexia ? bodyEl.classList.add('dyslexia') : bodyEl.classList.remove('dyslexia');
-    setThemeDyslexiaState({ ...themeDyslexiaState, isDyslexia });
-  };
+    const isDyslexia = !themeDyslexiaState.isDyslexia
+    localStorage.setItem('isDyslexia', JSON.stringify(isDyslexia))
+    const bodyEl = document.querySelector('body')
+    isDyslexia
+      ? bodyEl.classList.add('dyslexia')
+      : bodyEl.classList.remove('dyslexia')
+    setThemeDyslexiaState({ ...themeDyslexiaState, isDyslexia })
+  }
 
   return (
     <ThemeDyslexiaContext.Provider
       value={{
         isDyslexia: themeDyslexiaState.isDyslexia,
-        toggleDyslexiaTheme
+        toggleDyslexiaTheme,
       }}
     >
       {children}
     </ThemeDyslexiaContext.Provider>
-  );
-};
+  )
+}
 
-export { ThemeDyslexiaProvider, useDyslexiaTheme };
+export { ThemeDyslexiaProvider, useDyslexiaTheme }
