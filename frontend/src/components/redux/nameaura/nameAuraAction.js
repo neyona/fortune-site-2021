39cf7add0d783ca767faf4949f0393fd7769451d
neyona <40152041+neyona@users.noreply.github.com>
2021-04-24 18:13:48 -0400

    Update with lint fixes

--- frontend/src/components/redux/nameaura/nameAuraAction.js
+++ frontend/src/components/redux/nameaura/nameAuraAction.js
@@ -1,9 +1,9 @@
-// fortune_docker/frontend/src/components/redux/nameaura/fortuneAction.js
-import { NAME_AURA } from './types';
+// frontend/src/components/redux/nameaura/fortuneAction.js
+import { NAME_AURA } from './types'
 
 export function getNameAura(nameAura) {
-    return {
-        type: NAME_AURA,
-        payload: [nameAura]
-    };
-};
+  return {
+    type: NAME_AURA,
+    payload: [nameAura],
+  }
+}
