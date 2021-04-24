fa0d8a20e584a2340ec6db229da3120802332cef
neyona <40152041+neyona@users.noreply.github.com>
2021-04-24 17:48:09 -0400

    Updating the error page with lint fixes

--- frontend/src/components/pages/ErrorPage.js
+++ frontend/src/components/pages/ErrorPage.js
@@ -1,9 +1,9 @@
-// fortune_docker/frontend/src/components/pages/ErrorPage.js
-import React, { Component } from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Figure from 'react-bootstrap/Figure';
+// frontend/src/components/pages/ErrorPage.js
+import React, { Component } from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Figure from 'react-bootstrap/Figure'
 
-import FirstPageCuttleFishEye from '../assets/FirstPageCuttlefishEye.jpg';
+import FirstPageCuttleFishEye from '../assets/FirstPageCuttlefishEye.jpg'
 
 // This error page comes up at any page that is not the frontend. Backend
 // pages must be typed in correctly to not get this page.
@@ -14,20 +14,25 @@ export default class ErrorPage extends Component {
         <Jumbotron className="jumbotron-page">
           <h2>HOW DID YOU GET HERE!</h2>
           <h3>404 ERROR</h3>
-            <Figure>
-              <Figure.Image
-                width={300}
-                height={400}
-                alt="A Cuttlefish eye close up from the side"
-                src={FirstPageCuttleFishEye}
-                className="img"
-              />
-              <Figure.Caption className="figure-caption-custom">
-                It's me, I see you!
-              </Figure.Caption>
-            </Figure>
-          <h2>This is not the page you are looking for. There is no fortune here.</h2>
-          <p>You can use the navigation menu up top to get back to non-404 error pages.</p>
+          <Figure>
+            <Figure.Image
+              width={300}
+              height={400}
+              alt="A Cuttlefish eye close up from the side"
+              src={FirstPageCuttleFishEye}
+              className="img"
+            />
+            <Figure.Caption className="figure-caption-custom">
+              It's me, I see you!
+            </Figure.Caption>
+          </Figure>
+          <h2>
+            This is not the page you are looking for. There is no fortune here.
+          </h2>
+          <p>
+            You can use the navigation menu up top to get back to non-404 error
+            pages.
+          </p>
         </Jumbotron>
       </div>
     )
