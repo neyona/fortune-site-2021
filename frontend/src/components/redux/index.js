9354aad9b27e4e9bba21e5625c5f5eb5bd788542
neyona <40152041+neyona@users.noreply.github.com>
2021-04-24 18:12:53 -0400

    Updating with lint fixes

--- frontend/src/components/redux/index.js
+++ frontend/src/components/redux/index.js
@@ -1,8 +1,8 @@
-// fortune_docker/frontend/src/components/redux/index.js
-import { combineReducers } from 'redux';
+// frontend/src/components/redux/index.js
+import { combineReducers } from 'redux'
 
-import nameAuraReducer from './nameaura/nameAuraReducer';
+import nameAuraReducer from './nameaura/nameAuraReducer'
 
 export default combineReducers({
-    nameAura: nameAuraReducer,
-});
+  nameAura: nameAuraReducer,
+})
