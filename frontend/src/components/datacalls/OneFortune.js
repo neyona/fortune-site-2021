cbd917807a72610618b3155b884497e6e4e99c08
neyona <40152041+neyona@users.noreply.github.com>
2021-04-24 17:04:36 -0400

    After lint fix

--- frontend/src/components/datacalls/OneFortune.js
+++ frontend/src/components/datacalls/OneFortune.js
@@ -1,45 +1,46 @@
-// fortune_docker/frontend/src/components/datacalls/OneFortune.js
-import React, { Component } from "react";
-import { useQuery } from "react-query";
-import regeneratorRuntime from 'regenerator-runtime';
-
+// frontend/src/components/datacalls/OneFortune.js
+import React, { Component } from 'react'
+import { useQuery } from 'react-query'
+import regeneratorRuntime from 'regenerator-runtime'
 
 export default function OneFortune() {
-
-  const apiUrl = process.env.API_URL_FSS;
-  const FORTUNE_URL = `${apiUrl}`;
-  const TEST_KEY = process.env.TEST_KEY;
+  const apiUrl = process.env.API_URL_FSS
+  const FORTUNE_URL = `${apiUrl}`
+  const TEST_KEY = process.env.TEST_KEY
 
   // If fetch or this function stops working, regenerate and update the API Keys
   // The following is set up for samesite
   const fetchFortunes = async (key, checking) => {
-    const config = {'X-Api-Key': TEST_KEY};
-    const res = await fetch(FORTUNE_URL, {headers: config});
-    return res.json([]);
+    const config = { 'X-Api-Key': TEST_KEY }
+    const res = await fetch(FORTUNE_URL, { headers: config })
+    return res.json([])
   }
 
-  const { data, status } = useQuery(['allfortunes', 'I fetched'], fetchFortunes, {retry: true});
+  const { data, status } = useQuery(
+    ['allfortunes', 'I fetched'],
+    fetchFortunes,
+    { retry: true }
+  )
 
-  let funFortunes = data;
+  let funFortunes = data
 
   if (status === 'loading') {
-    return <span>, lemme get that fortune for you . . .</span>;
+    return <span>, lemme get that fortune for you . . .</span>
   } else if (status === 'error') {
-    return <span> Error fetching data</span>;
+    return <span> Error fetching data</span>
   } else if (status === 'success' && Array.isArray(funFortunes) == true) {
-    const fortuneItems = funFortunes.map(fortune => (
+    const fortuneItems = funFortunes.map((fortune) => (
       <span key={fortune.title}>{fortune.body}</span>
-    ));
-    const randomId = Math.floor(Math.random() * fortuneItems.length);
+    ))
+    const randomId = Math.floor(Math.random() * fortuneItems.length)
     return (
       <>
         <span>{fortuneItems[randomId]}</span>
       </>
-    );
-  } else if ((status === 'success' && Array.isArray(funFortunes) == false))
-  {
-    return <span>, funFortunes was not an array.</span>;
+    )
+  } else if (status === 'success' && Array.isArray(funFortunes) == false) {
+    return <span>, funFortunes was not an array.</span>
   } else {
-    return <span>, there is an unspecified issue.</span>;
+    return <span>, there is an unspecified issue.</span>
   }
 }
